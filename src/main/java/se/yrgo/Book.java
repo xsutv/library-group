f244d380de09b6fe47e996b42812eab439d74c7c
Haimen <haimenabbas@gmail.com>
2024-12-06 10:12:58 +0100

    added addBook in library

--- src/main/java/se/yrgo/Book.java
+++ src/main/java/se/yrgo/Book.java
@@ -1,5 +1,40 @@
 package se.yrgo;
 
 public class Book {
+
+    private String author;
+    private String title;
+    private String isbn;
+    private String genre; // change this to an enum later.
+    private int pages;
+
+    public Book(String author, String title, String isbn, String genre, int pages) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+        this.genre = genre;
+        this.pages = pages;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public int getPages() {
+        return pages;
+    }
+
+
+    
+    
     
 }
--- src/main/java/se/yrgo/Library.java
+++ src/main/java/se/yrgo/Library.java
@@ -1,5 +1,22 @@
 package se.yrgo;
 
+import java.util.*;
+
 public class Library {
     
+    Book book;
+    private List<Book> library = new ArrayList<>();
+
+    public void addBook(Book book) {
+        for (Book b : library) {
+            if (b.getIsbn().equals(book.getIsbn())) {
+                System.out.println("Book already exists.");
+            } else {
+                library.add(book);
+            }
+        }
+    }
+
+    
+
 }
