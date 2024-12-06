d94591c695ea63c340dc4737f362dcc8d2bb133e
Haimen <haimenabbas@gmail.com>
2024-12-06 14:34:23 +0100

    merge local branches

--- src/main/java/se/yrgo/models/Library.java
+++ src/main/java/se/yrgo/models/Library.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class Library {
     
-    Book book;
+    private Book book;
     private List<Book> library = new ArrayList<>();
 
     public void addBook(Book book) {
         for (Book b : library) {
-            if (b.getIsbn().equals(book.getIsbn())) {
+            if (b.getIsbn().equals(this.book.getIsbn())) {
                 System.out.println("Book already exists.");
             } else {
-                library.add(book);
+                library.add(this.book);
             }
         }
     }
