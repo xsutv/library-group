7f6b07d51d204638735a14845e5d66961c2e3248
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2024-12-09 10:34:17 +0100

    add Books to library

--- src/main/java/se/yrgo/models/Book.java
+++ src/main/java/se/yrgo/models/Book.java
@@ -8,33 +8,32 @@ public class Book {
     private String genre; // change this to an enum later.
     private int pages;
 
-    public Book(String author, String title, String isbn, String genre, int pages) {
-        this.author = author;
-        this.title = title;
+    public Book(String isbn, String author, String title, String genre, int pages) {
         this.isbn = isbn;
+        this.title = title;
+        this.author = author;
         this.genre = genre;
         this.pages = pages;
     }
 
-
     public String getAuthor() {
         return author;
     }
+
     public String getTitle() {
         return title;
     }
+
     public String getIsbn() {
         return isbn;
     }
+
     public String getGenre() {
         return genre;
     }
+
     public int getPages() {
         return pages;
     }
 
-
-    
-    
-    
 }
