6f49e87f1ced491cbfa740a2cd37ea9847d9484a
Haimen <haimenabbas@gmail.com>
2024-12-09 12:27:24 +0100

    Added review and reservation

--- src/main/java/se/yrgo/models/Library.java
+++ src/main/java/se/yrgo/models/Library.java
@@ -5,11 +5,8 @@ import java.util.*;
 public class Library {
 
     private Book book;
-    private List<Book> library = new ArrayList<>(List.of(new Book("9780008511685", "Butter", "Asako Yuzuki", "", 450),
-            new Book("9781405950084", "Black Cake", "Charmaine Wikerson", "", 432),
-            new Book("9781784700447", "The Memory Police", "Yoko Ogawa", "", 288),
-            new Book("9781529029581", "Before the Coffee Gets Cold", "Toshikazu Kawaguchi", "", 224),
-            new Book("1529050863", "Tales from the Cafe", "Toshikazu Kawaguchi", "", 256)));
+
+    private List<Book> library = new ArrayList<>();
 
     public void addBook(Book book) {
         for (Book b : library) {
@@ -20,5 +17,7 @@ public class Library {
             }
         }
     }
+
+    
 
 }
--- src/main/java/se/yrgo/ui/Main.java
+++ src/main/java/se/yrgo/ui/Main.java
@@ -1,7 +1,21 @@
 package se.yrgo.ui;
 
+import java.util.*;
+
+import se.yrgo.models.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+
+            List<Book> library = new ArrayList<>(List.of(new Book("9780008511685", "Butter", "Asako Yuzuki", "", 450),
+            new Book("9781405950084", "Black Cake", "Charmaine Wikerson", "", 432),
+            new Book("9781784700447", "The Memory Police", "Yoko Ogawa", "", 288),
+            new Book("9781529029581", "Before the Coffee Gets Cold", "Toshikazu Kawaguchi", "", 224),
+            new Book("1529050863", "Tales from the Cafe", "Toshikazu Kawaguchi", "", 256)));
+
+        
+            // Library.addBook();
+
+
     }
 }
