d4e259bca4eaca19686289b53534ae3295286707
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2024-12-18 13:15:47 +0100

    added comment on Loans test (Viggo added but through Fias laptop)

--- src/test/java/se/yrgo/models/LoanTest.java
+++ src/test/java/se/yrgo/models/LoanTest.java
@@ -1,7 +1,8 @@
 package se.yrgo.models;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 public class LoanTest {
     private Library lib = new Library();
@@ -9,10 +10,11 @@ public class LoanTest {
 
     private Book book1 = new Book("9789113084909", "Ringens brödraskap", "J R R Tolkien", 528, "fantasy");
     private User user1 = new User("1234", "viggo", "nielsen", "trumpetvägen 12", "viggo@gmail.com");
-    private User user2 = new User("5678", "Qbert", "jansson", "vikingagatan 15", "qbert@outlook.com");
-
 
     @Test
+    /**
+     * @author Viggo
+     */
     public void testLoanBook() {
         isbn = "9789113084909";
         lib.addBook(book1);
@@ -23,10 +25,13 @@ public class LoanTest {
     }
 
     @Test
+    /**
+     * @author Viggo
+     */
     public void testCheckLoanStatus() {
         // true = book not borrowed
         Loan.loanBook(lib.getLib(), isbn, user1);
-        assertTrue(Loan.checkLoanStatus(book1.getIsbn(), user1));
+        assertFalse(Loan.checkLoanStatus(book1.getIsbn(), user1));
     }
 
 }
